2fc41ccf36a5613dd9cbbdb534f94d53b2beee19
Hibikine Kage <goodsmile314159@gmail.com>
2018-11-21 01:31:12 +0900

    tmp

--- Bar/index.tsx
+++ Bar/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import bar from '../assets/img/bar.png';
-import { Texture, Point, SCALE_MODES, BaseRenderTexture } from 'pixi.js';
+import { Texture, Point, SCALE_MODES } from 'pixi.js';
 import { Sprite, SpriteProperties } from 'react-pixi-fiber';
 import { resource } from '../ResourceContext';
 import { orangeSize } from '../Orange/consts';
@@ -11,7 +11,7 @@ interface Props extends SpriteProperties {
 
 const anchor = new Point(0.5, 1);
 const Bar = (props: Props) => {
-  const s = orangeSize / props.barTexture.baseTexture.realHeight;
+  const s = orangeSize / 4 / props.barTexture.baseTexture.realHeight;
   const scale = new Point(s, s);
   props.barTexture.baseTexture.scaleMode = SCALE_MODES.NEAREST;
   return (
